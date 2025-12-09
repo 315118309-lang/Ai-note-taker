b79730e8da65900c0a257bd11c04993b2c0ca3f3
315118309-lang <315118309@qq.com>
2025-12-09 12:00:21 +0800

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // 导入全局样式
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
